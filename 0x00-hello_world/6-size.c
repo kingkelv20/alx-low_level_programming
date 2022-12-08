602ea2d810bcd2e301f3da7a997612b3c37effe8
kingkelv20 <kelvinagimogim@gmail.com>
2022-12-08 13:17:34 -0800

    task5,6,7,and 8

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+/**
+ * main -Entry
+ * Return: Always 0
+ */
+Int main(void)
+{
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of a char: %i byte(s)\n", sizeof(int));
+	printf("Size of a char: %i byte(s)\n", sizeof(long int));
+	printf("Size of a char: %i byte(s)\n", sizeof(float));
+
+	return (0);
+}
