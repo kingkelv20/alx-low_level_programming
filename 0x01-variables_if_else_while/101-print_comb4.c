64f4dbb6da78b70be6603dd974a8bb044ea841eb
kingkelv20 <kelvinagimogim@gmail.com>
2022-12-10 13:38:37 -0800

    trails

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,27 +15,30 @@ int main(void)
 
 	while (i < 58)
 	{
-		k = j + 1;
-		while (k < 58)
-		{
-			putchar(i);
-			putchar(j);
-			putchar(k);
-			if (i < 55 || j < 56 || k < 57)
-			{
-				putchar(44);
-				putchar(32);
+		j = i + 1;
+		while (j < 58)
+		{ 
+			 k = j + 1;
+			 while (k < 58)
+			 {
+				 putchar(i);
+				 putchar(j);
+				 putchar(k);
+				 if (i < 55 || j < 56 || k < 57)
+				 {
+					 putchar(44);
+					 putchar(32);
+				 }
+				 k++;
 			}
-			k++;
-		}
-		
-		j++;
+
+			 j++;
+	       }
+
+		i++;
 	}
 
-	i++;
-}
+	putchar(10);
 
-putchar(10);
-
-return (0);
+	return (0);
 }
