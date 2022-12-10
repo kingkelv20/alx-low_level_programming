af9629d80c76b5ceb540c12cb60ed6909a3cb196
kingkelv20 <kelvinagimogim@gmail.com>
2022-12-10 12:55:37 -0800

    numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i, j k;
+	i = 48;
+	j = 48;
+	k = 48;
+
+	while (i < 58)
+	{
+		k = j + 1;
+		while (k < 58)
+		{
+			putchar(i);
+			putchar(j);
+			putchar(k);
+			if (i < 55 || j < 56 || k < 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			k++;
+		}
+		
+		j++;
+	}
+
+	i++;
+}
+
+putchar(10);
+
+return (0);
+}
