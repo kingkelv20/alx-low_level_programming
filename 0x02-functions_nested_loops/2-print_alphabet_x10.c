36ef69babd524c8781a568c07c8326a3801404b1
kingkelv20 <kelvinagimogim@gmail.com>
2022-12-13 12:32:18 -0800

    2-print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,17 @@
-/*
- * File: 2-print_alphabet_x10.c
- * * Auth: kelvin Agimogim
- */
-
-
-#include "holberton.h"
+#include "main.h"
 
 /**
  * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
  */
 void print_alphabet_x10(void)
 {
-	for (int i = 0; i < 10; i++)
+	int count = 0;
+	char letter;
+
+	while (count++ <= 9)
 	{
-		for (char ch = 'a'; ch <= 'z'; ch++)
-		{
-			_putchar(ch);
-		}
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
 		_putchar('\n');
 	}
 }
