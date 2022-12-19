41d6a101488afac5e9d5431cc6ecda44657576d3
kingkelv20 <kelvinagimogim@gmail.com>
2022-12-19 01:20:42 -0800

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,34 +1,23 @@
 #include "main.h"
 
 /**
- * rev_string - Reverses a string
- * @s: The string to be modified
+ * string_toupper - Changes all lowercase letters
+ *                  of a string to uppercase.
+ * @str: The string to be changed.
  *
- *  Return: void
+ * Return: A pointer to the changed string.
  */
-void rev_string(char *s)
+char *string_toupper(char *str)
 {
-	int i, c, k;
-	char *a, aux;
+	int index = 0;
 
-	a = s;
-
-	while (s[c] != '\0')
+	while (str[index])
 	{
-		c++;
+		if (str[index] >= 'a' && str[index] <= 'z')
+			str[index] -= 32;
+
+		index++;
 	}
 
-	for (k = 1; k < c; k++)
-	{
-		a++;
-	}
-
-	for (i = 0; i < (c / 2); i++)
-	{
-		aux = s[i];
-		s[i] = *a;
-		*a = aux;
-		a--;
-	}
+	return (str);
 }
-
