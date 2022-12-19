70f81d3a2f6579be7777409e2b38fcb573bd33a6
kingkelv20 <kelvinagimogim@gmail.com>
2022-12-19 02:42:48 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - generates keygen.
+ *  Return: 0 Always.
+ */
 int main(void)
 {
-	int n;
-	 int a[5];
-	 int *p;
+	int r = 0, c = 0;
+	time_t t;
 
-	 a[2] = 1024;
-	 p = &n;
-	 /*
-	  * write your line of code here...
-	  * Remember:
-	  * - you are not allowed to use a
-	  *   - you are not allowed to modify p
-	  *    - only one statement
-	  *     - you are not allowed to code anything else than this line of code
-	  */
-	   *(p + 5) = 98;
-	   /* ...so that this prints 98\n */
-	   printf("a[2] = %d\n", a[2]);
-	    return (0);
+	srand((unsigned int) time(&t));
+	while (c < 2772)
+	{
+		r = rand() % 128;
+		if ((c + r) > 2772)
+			break;
+		c = c + r;
+		printf("%c", r);
+	}
+	printf("%c\n", (2772 - c));
+	return (0);
 }
