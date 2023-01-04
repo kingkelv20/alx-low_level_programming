63be959e4eb19b5f6ac289579189a6a13c6d0744
kingkelv20 <kelvinagimogim@gmail.com>
2023-01-03 23:36:53 -0800

    return (check_palindrome(s, len, index));

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,43 +1,62 @@
 #include "main.h"
 
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+int is_palindrome(char *s);
+
 /**
- * is_palindrome - Returns 1 if string is a palindrome or 0 if not
- * @s: string
- * Return: 1 or 0.
+ * find_strlen - Returns the length of a string.
+ * @s: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
+int find_strlen(char *s)
+{
+	int len = 0;
+
+	if (*(s + len))
+	{
+		len++;
+		len += find_strlen(s + len);
+	}
+
+	return (len);
+}
+
+/**
+ * check_palindrome - Checks if a string is a palindrome.
+ * @s: The string to be checked.
+ * @len: The length of s.
+ * @index: The index of the string to be checked.
+ *
+ * Return: If the string is a palindrome - 1.
+ *         If the string is not a palindrome - 0.
+ */
+int check_palindrome(char *s, int len, int index)
+{
+	if (s[index] == s[len / 2])
+		return (1);
+
+	if (s[index] == s[len - index - 1])
+		return (check_palindrome(s, len, index + 1));
+
+	return (0);
+}
+
+/**
+ * is_palindrome - Checks if a string is a palindrome.
+ * @s: The string to be checked.
+ *
+ * Return: If the string is a palindrome - 1.
+ *         If the string is not a palindrome - 0.
  */
 int is_palindrome(char *s)
 {
-	int first, last;
+	int index = 0;
+	int len = find_strlen(s);
 
-	first = 0;
-	last = _strlen_recursion(s) - 1;
-	return (palindrome(s, first, last));
-}
-/**
- * palindrome - third party function
- * @s: string to be tested
- * @first: integer for iteration and comparison
- * @last: integer for iteration and comparison
- *
- * Return: 1 or 0;
- */
-int palindrome(char *s, int first, int last)
-{
-	if (first > last)
+	if (!(*s))
 		return (1);
-	else if (s[first] == s[last])
-		return (palindrome(s, first + 1, last - 1));
-	else
-		return (0);
-}
-/**
- * _strlen_recursion - helps get string length recursively
- * @s: string
- * Return: 0
- */
-int _strlen_recursion(char *s)
-{
-	if (*s != '\0')
-		return (1 + _strlen_recursion(s + 1));
-	return (0);
+
+	return (check_palindrome(s, len, index));
 }
